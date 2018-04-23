d8777e24da383d43d19fa6f436e0507186475e51
DESKTOP-LK4POK4\Judit <toth.jdt@gmail.com>
2018-04-23 16:52:39 +0200

    bg

--- middleware/data/addData.js
+++ middleware/data/addData.js
@@ -5,9 +5,13 @@ var fs = require('fs');
 
 function dbconnect(callback, data) {
 
-    connection.query('INSERT INTO food (kaja,leiras,ar,type) VALUES(' +
+    connection.query('INSERT INTO food (kaja, kaja_EN, kaja_D, leiras, leiras_EN, leiras_D, ar,type) VALUES(' +
         mysql.escape(data.etel) + ',' +
+        mysql.escape(data.etel_EN) + ',' +
+        mysql.escape(data.etel_D) + ',' +
         mysql.escape(data.leiras) + ',' +
+        mysql.escape(data.leiras_EN) + ',' +
+        mysql.escape(data.leiras_D) + ',' +
         mysql.escape(data.ar) + ',' +
         mysql.escape(data.type) + ')',
         function(err,rows){
--- middleware/data/updateData.js
+++ middleware/data/updateData.js
@@ -3,8 +3,16 @@ var connection = require('../../config/config');
 
 function dbconnect(callback, data, id) {
 
-    connection.query('UPDATE food SET kaja = ' + mysql.escape(data.etel) + ', leiras = ' + mysql.escape(data.leiras) +
-        ', ar = ' + mysql.escape(data.ar) + ', type = ' + mysql.escape(data.type) + ' WHERE id = ' + id,
+    connection.query('UPDATE food SET kaja = '
+        + mysql.escape(data.etel) + ', kaja_EN = '
+        + mysql.escape(data.etel_EN) + ', kaja_D = '
+        + mysql.escape(data.etel_D) + ', leiras = '
+        + mysql.escape(data.leiras) + ', leiras_EN = '
+        + mysql.escape(data.leiras_EN) + ', leiras_D = '
+        + mysql.escape(data.leiras_D) + ', ar = '
+        + mysql.escape(data.ar) + ', type = '
+        + mysql.escape(data.type) + ' WHERE id = '
+        + id,
         function(err,row){
             if (err) {
                 throw err;
