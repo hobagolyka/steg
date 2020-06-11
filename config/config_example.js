45f7712bf3a61680be22a7fc05ad9f7c94890e17
Judit <toth.jdt@gmail.com>
2020-06-11 13:05:08 +0200

    new menu

--- config/config_example.js
+++ config/config_example.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host     : '',
     user     : '',
     password : '',
@@ -8,3 +8,4 @@ var connection = mysql.createConnection({
 });
 
 module.exports = connection;
+
--- middleware/generic/render.js
+++ middleware/generic/render.js
@@ -1,8 +1,10 @@
+var googleApiKey = require('../../config/googleApi');
 
 module.exports = function (viewName, cim) {
 
     return function (req, res, next) {
         res.tpl.title = cim;
+        res.tpl.googleApi = "https://maps.googleapis.com/maps/api/js?key=" + googleApiKey + "&callback=initMap";
         res.render(viewName, res.tpl);
     };
 
